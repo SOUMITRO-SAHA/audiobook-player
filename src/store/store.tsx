45b2e756095b293cba6a966b92963e82e7dbfb2f
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-07-30 02:43:49 +0530

    Redux Stores Updated for Library

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import accountReducer from "./slice/account";
 import folderReducer from "./slice/folder";
+import libraryReducer from "./slice/library";
 import { Provider } from "react-redux";
 
 const RootReducer = combineReducers({
   account: accountReducer,
   folder: folderReducer,
+  library: libraryReducer,
 });
 
 const store = configureStore({
