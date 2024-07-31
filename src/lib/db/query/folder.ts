2345f11c493558b838a077ba69d62f961de89f35
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-07-31 12:26:28 +0530

    Adding New Folder Logic Updated

--- src/lib/db/query/folder.ts
+++ src/lib/db/query/folder.ts
@@ -35,8 +35,9 @@ export const fetchFolderByFolderId = async (folderId: number) => {
 export const fetchAllFilesByFolderId = async (folderId: number) => {
   try {
     const response = await db.query.track.findMany({
-      where: eq(track.folderId, folderId),
+      where: eq(track.dbFolderId, folderId),
     });
+
     if (response.length > 0) return response;
     return null;
   } catch (error) {
@@ -60,21 +61,24 @@ export const deleteFolderByFolderId = async (folderId: number) => {
     }
 
     // Delete tracks from the database
-    await db.delete(track).where(eq(track.folderId, folderId));
+    await db.delete(track).where(eq(track.dbFolderId, folderId));
 
     // Delete the folder from the database
-    await db.delete(folders).where(eq(folders.id, folderId));
+    const res = await db.delete(folders).where(eq(folders.id, folderId));
 
-    const msg = `Folder with ID ${folderId} and its contents have been deleted successfully.`;
-    console.log(msg);
+    if (res) {
+      const msg = `Folder with ID ${folderId} and its contents have been deleted successfully.`;
 
-    ToastAndroid.show(msg, ToastAndroid.SHORT);
+      console.log(msg);
+      ToastAndroid.show(msg, ToastAndroid.SHORT);
+    }
+
+    return res;
   } catch (error) {
     console.error("Error deleting folder:", error);
     if (error instanceof Error) {
       ToastAndroid.show(error.message, ToastAndroid.SHORT);
     }
-
     throw error;
   }
 };
