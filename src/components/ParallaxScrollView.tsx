21ba2d732eca203057aa53b1ff34b4a7f17eb4b7
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-08-04 22:26:35 +0530

    UI Enhancement

--- src/components/ParallaxScrollView.tsx
+++ src/components/ParallaxScrollView.tsx
@@ -1,16 +1,21 @@
 import { ThemedView } from "@/components/ThemedView";
 import { cn } from "@/lib/utils";
 import type { PropsWithChildren } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, ViewStyle } from "react-native";
 
 type Props = PropsWithChildren<{
   className?: string;
   gap?: number;
+  style?: ViewStyle;
 }>;
 
-export default function ParallaxScrollView({ children, className }: Props) {
+export default function ParallaxScrollView({
+  children,
+  className,
+  style,
+}: Props) {
   return (
-    <ThemedView className={cn("flex-1", className)}>
+    <ThemedView className={cn("flex-1", className)} style={[style]}>
       <ThemedView
         style={styles.content}
         className={cn("mt-8 flex-1 p-8 bg-background overflow-hidden")}
