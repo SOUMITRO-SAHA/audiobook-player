f2ae267f249e7ba80b37a704f19cddf767b0f2d4
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-08-05 22:27:18 +0530

    WIP

--- src/components/player/player-features.tsx
+++ src/components/player/player-features.tsx
@@ -1,8 +1,46 @@
-import React from "react";
+import * as React from "react";
 import { ThemedText } from "../ThemedText";
+import { View } from "react-native";
+import TrackPlayer from "react-native-track-player";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Colors } from "@/constants";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { TouchableOpacity } from "@gorhom/bottom-sheet";
 
 const PlayerFeatures = () => {
-  return <ThemedText>PlayerFeatures</ThemedText>;
+  const [currentRate, setCurrentRate] = React.useState(1);
+
+  // Side Effects
+  React.useEffect(() => {
+    (async () => {
+      const rate = await TrackPlayer.getRate();
+      if (rate) {
+        setCurrentRate(rate);
+      }
+    })();
+  }, []);
+
+  return (
+    <View className="flex flex-row mt-6 -mb-8 justify-evenly">
+      <TouchableOpacity activeOpacity={0.8}>
+        <View className="items-center justify-center w-20 h-16 p-3 shadow rounded-xl bg-black/50">
+          <ThemedText>{currentRate}</ThemedText>
+          <ThemedText>Speed</ThemedText>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity activeOpacity={0.8}>
+        <View className="flex items-center justify-center w-20 h-16 p-3 shadow rounded-xl bg-black/50">
+          <MaterialCommunityIcons
+            name="timer-outline"
+            size={24}
+            color={Colors.dark.foreground}
+          />
+          <ThemedText>Timer</ThemedText>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default PlayerFeatures;
