80bc4ba0bd7b3ad98c002e3a406086ffff3043d3
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-08-05 22:27:09 +0530

    Refactor

--- src/components/player/player-advance-controller.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ThemedText } from "../ThemedText";
-
-const PlayerAdvanceController = () => {
-  return <ThemedText>PlayerAdvanceController</ThemedText>;
-};
-
-export default PlayerAdvanceController;
--- src/components/player/player-progress-bar.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ThemedText } from "../ThemedText";
-
-const PlayerProgressBar = () => {
-  return <ThemedText>PlayerProgressBar</ThemedText>;
-};
-
-export default PlayerProgressBar;
