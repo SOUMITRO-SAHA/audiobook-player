21e528e0085f5f09138293f8059f39694d17c57f
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-08-11 20:16:57 +0530

    Some Constants Updated

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -2,3 +2,4 @@ export const DEFAULT_DATABASE_NAME: string = "audiobook.db";
 export const DEFAULT_RADIUS: number = 0.25;
 export const DEFAULT_FOLDER_NAME: string = "Audiobooks";
 export const EXTERNAL_DIRECTORY_URL = "file:///storage/emulated/0/";
+export const DEFAULT_AUDIOBOOK_FOLDER_URI = `${EXTERNAL_DIRECTORY_URL}/${DEFAULT_FOLDER_NAME}`;
