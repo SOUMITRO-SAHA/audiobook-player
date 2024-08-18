71fee657e191a168f9b14a4ee8afb2e24cd5b306
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-08-18 14:09:43 +0530

    updating the primary colors and secondary colors

--- src/hooks/useBackgroundImageColor.ts
+++ src/hooks/useBackgroundImageColor.ts
@@ -5,8 +5,29 @@ import { getColors, ImageColorsResult } from "react-native-image-colors";
 type ColorType = {
   primary: string;
   secondary: string;
+  tertiary?: string;
   raw: ImageColorsResult;
 };
+
+/**
+  Platform `Android`
+  
+  const demo = {
+    primary: "#506088",
+    raw: {
+      average: "#232125",
+      darkMuted: "#506088",
+      darkVibrant: "#486898",
+      dominant: "#305080",
+      lightMuted: "#8090B8",
+      lightVibrant: "#F9991C",
+      muted: "#5070A8",
+      platform: "android",
+      vibrant: "#305080",
+    },
+    secondary: "#5070A8",
+  };
+*/
 
 export const useBackgroundImageColor = (url: string) => {
   const [colors, setColors] = React.useState<ColorType | null>(null);
@@ -23,22 +44,24 @@ export const useBackgroundImageColor = (url: string) => {
       switch (value.platform) {
         case "android":
           setColors({
-            primary: value.darkMuted,
-            secondary: value.muted,
+            primary: value.lightVibrant,
+            secondary: value.vibrant,
+            tertiary: value.muted,
             raw: value,
           });
           break;
         case "web":
           setColors({
             primary: value.darkMuted,
-            secondary: value.muted,
+            secondary: value.lightVibrant,
+            tertiary: value.muted,
             raw: value,
           });
           break;
         case "ios":
           setColors({
             primary: value.background,
-            secondary: value.detail,
+            secondary: value.secondary,
             raw: value,
           });
         default:
