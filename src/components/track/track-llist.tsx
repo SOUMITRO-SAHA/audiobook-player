6cb505ac29be3423e19b941255295e16fbdc0daf
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-08-19 22:04:23 +0530

    Implemented playing any music from the playlist with the playlist dynamically starts the queue

--- src/components/track/track-llist.tsx
+++ src/components/track/track-llist.tsx
@@ -1,21 +1,50 @@
 import { Asset } from "expo-media-library";
-import React from "react";
+import * as React from "react";
 import { FlatList } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-import { ThemedText } from "../ThemedText";
-import { ThemedView } from "../ThemedView";
+import { ThemedText, ThemedView } from "@/components";
 import { TrackListItem } from "./track-player-item";
+import { useQueue } from "@/store/queue";
+import TrackPlayer from "react-native-track-player";
+import { addTracks } from "@/lib/services/track-player-helper";
 
-export const TrackList = ({
-  trackList,
-  refreshing = false,
-  handleRefresh,
-}: {
+type TrackListProps = {
+  id: string;
   refreshing: boolean;
   trackList: Asset[];
   handleRefresh: () => void;
+};
+
+export const TrackList: React.FC<TrackListProps> = ({
+  id,
+  trackList,
+  refreshing = false,
+  handleRefresh,
+  ...flatlistProps
 }) => {
+  const queueOffSet = React.useRef(0);
+  const { activeQueueId, setActiveQueueId } = useQueue();
+
+  const handleTrackSelect = async (selectedTrack: Asset) => {
+    const trackIndex = trackList.findIndex(
+      (track) => track.id === selectedTrack.id
+    );
+
+    if (trackIndex === -1) return;
+
+    const beforeTracks = trackList.slice(0, trackIndex);
+    const afterTracks = trackList.slice(trackIndex + 1);
+
+    const updatedTracks = [selectedTrack, ...afterTracks, ...beforeTracks];
+
+    // Now adding this track-object into the tracks
+    await addTracks({ assets: updatedTracks });
+
+    queueOffSet.current = trackIndex;
+    setActiveQueueId(id);
+  };
+
   return (
     <FlatList
       style={{
@@ -23,7 +52,11 @@ export const TrackList = ({
       }}
       data={trackList}
       renderItem={({ item, index }) => (
-        <TrackListItem track={item} index={index + 1} />
+        <TrackListItem
+          track={item}
+          index={index + 1}
+          handleTrackSelect={handleTrackSelect}
+        />
       )}
       ListFooterComponent={() => {
         if (trackList.length > 0) {
